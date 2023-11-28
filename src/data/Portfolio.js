c1fe1987b3d68935e7794d8c65f6b35fb23599db
jabirfarah <86932547+jabirfarah@users.noreply.github.com>
2023-11-28 14:49:02 +0000

    Add new project to portfolio

--- src/data/Portfolio.js
+++ src/data/Portfolio.js
@@ -1,9 +1,21 @@
 export default [
   {
+    title: "Coco.",
+    imgUrl: "./assets/Coco.png",
+    stack: ["React", "Tailwind CSS", "JavaScript", "NodeJS", "GraphQL", "RESTful API's"],
+    link: "https://coco-sand.vercel.app/",
+  },
+  {
     title: "MazeMathics",
     imgUrl: "./assets/mazemathics.png",
     stack: ["HTML", "CSS", "JavaScript", "NodeJS"],
     link: "https://github.com/jabirfarah/MazeMathics",
+  },
+  {
+    title: "Discord Geography Bot",
+    imgUrl: "./assets/medium.png",
+    stack: ["Python", "Discord API", "RESTful API"],
+    link: "https://github.com/jabirfarah/henry-discord-bot",
   },
   {
     title: "Portfolio Website",
@@ -11,14 +23,6 @@ export default [
     stack: ["React", "Tailwind CSS", "Vite"],
     link: "https://github.com/jabirfarah/portfolio",
   },
-
-  {
-    title: "Discord Geography Bot",
-    imgUrl: "./assets/medium.png",
-    stack: ["Python", "Discord API", "RESTful API"],
-    link: "https://github.com/jabirfarah/henry-discord-bot",
-  },
-
   {
     title: "DataGrowth",
     imgUrl: "https://raw.githubusercontent.com/jabirfarah/data-finance-website/main/src/assets/landingpage.png?token=GHSAT0AAAAAACCBPFE2BRMJNK7BKJGO4GMGZEGDX4A",
